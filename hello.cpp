9189db9992cb3fcf47a2f71d950d5074a41d6c7c
RICHA <92576300+200Richa@users.noreply.github.com>
2023-02-20 18:44:46 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "PES1UG20CS334\n";
+  cout << "Trying out Pipeline\n";
+  return 0;
+}
